f75d0cfaceb5f0021d7d24f2528fd89b548e08e2
jemz <nghiemj23@gmail.com>
2022-04-19 20:54:39 -0700

    create event listeners for each box to change to a random color

---
+++ my-js-file.js
@@ -0,0 +1,20 @@
+const canvas = document.querySelector('#canvas');
+
+for (let i = 0; i < 256; i +=1) {
+    const box = canvas.appendChild(document.createElement('div'));
+    box.classList.add('box')
+};
+
+function random(num) {
+    return Math.floor(Math.random()*(num+1));
+}
+
+const boxes = document.querySelectorAll('.box');
+
+for (i = 0; i < boxes.length; i +=1) {
+    boxes[i].addEventListener('mouseover', (e) => {
+        e.target.setAttribute('style', `background: rgb(${random(255)}, ${random(255)}, ${random(255)}`);
+        console.log(e.target);
+        });
+    };
+
